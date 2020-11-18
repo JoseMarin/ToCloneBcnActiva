f56a37af0dd9be69b65b5d39a09e603f92043450
Jose Marín <jmarinmalagon@gmail.com>
2020-11-18 17:02:15 +0100

    Update mainApp.java

--- AddYourName/src/mainApp.java
+++ AddYourName/src/mainApp.java
@@ -4,7 +4,7 @@ public class mainApp {
 	public static void main(String[] args) {
 		// Add your name here
 		System.out.println("Jose Marín - Dev 1");
-		System.out.println("Ferran Torres - Dev 2");
+		System.out.println("Ferran Torres - Dev 4");
 
 	}
 
